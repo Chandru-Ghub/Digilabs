e4195ed731674e8fb83406db30ece5bab0cd6532
Chandru-Ghub <chandruinfo455@gmail.com>
2024-01-11 03:28:15 +0530

    added

--- src/components/Admin/UpdatePage.js
+++ src/components/Admin/UpdatePage.js
@@ -37,7 +37,7 @@ const UpdatePage = () => {
         try {
             const update = await authAxios.post('/updatepage',logoImg?{title,buttonTxt,logoImg}:{title,buttonTxt})
             setOpen(false)
-            window.location.reload()
+            alert('Details updates! Refresh to see change')
 
         } catch (error) {
             console.log(error)
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react'
 import '../style/HeroSection.css'
-import axios from 'axios'
 const HeroSection = ({title,btn}) => {
 
   return (
